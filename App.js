ab9dead63f7a7b7078a4699c1a8a3913b6a5a8ce
Cole Fortner <colefortner@gmail.com>
2022-01-10 16:39:22 -0500

    My solution 95

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+import { colors } from "./src/utils/colors";
+import { spacing } from "./src/utils/sizes";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Testing</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.search}>
+        <Text>search</Text>
+      </View>
+      <View style={styles.list}>
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  search: {
+    backgroundColor: colors.green,
+    height: "5%",
+    padding: spacing.sm,
+    fontWeight: "bold"
+  },
+  list: {
+    backgroundColor: colors.blue,
+    height: "95%",
+    fontWeight: "bold",
+    padding: spacing.sm
+  }
+});
---
+++ src/utils/sizes.js
@@ -0,0 +1,4 @@
+export const spacing = {
+  sm: 10,
+  md: 20
+};
