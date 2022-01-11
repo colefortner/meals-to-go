36422bbf82301c4261ea505af00c89c68b1f461a
Cole Fortner <colefortner@gmail.com>
2022-01-11 16:13:34 -0500

    V108 - Set up RestaurantInfo component to take in a restaurant object/prop, destructure the restaurant object, provide the properties some default values and render the name property to test it

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -2,6 +2,17 @@ import React from "react";
 
 import { Text } from "react-native";
 
-export const RestaurantInfo = () => {
-  return <Text>Restaurant Info</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Pinellas Ale House",
+    icon,
+    photos = [
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgraziadaily.co.uk%2Fcelebrity%2Fnews%2Fkate-middleton%2F&psig=AOvVaw1alxTsBlnKE_vx5CnOSDDi&ust=1642021288439000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDJ747MqvUCFQAAAAAdAAAAABAn",
+    ],
+    address = "100 some random st",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text>{name}</Text>;
 };
