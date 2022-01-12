2efdc00ca5bfdd5af86a52d76d9c819f238947df
Cole Fortner <colefortner@gmail.com>
2022-01-11 21:16:57 -0500

    Video 111 Exercise Solution

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
@@ -16,9 +16,20 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant;
   return (
-    <Card>
-      <Card.Cover source={{ url: `${photos[0]}` }} style={{ padding: 20 }} />
-      <Card.Title title={name} />
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover
+        key={name}
+        style={styles.cover}
+        source={{ uri: photos[0] }}
+        style={{ padding: 20 }}
+      />
+      <Text style={styles.title}>{name}</Text>
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+  title: { padding: 16 },
+});
