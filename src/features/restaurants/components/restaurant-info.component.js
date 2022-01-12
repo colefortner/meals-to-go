1fd3d2fc45cec9390da6c564a038e98ddcaf19fb
Cole Fortner <colefortner@gmail.com>
2022-01-11 20:32:56 -0500

    Build basic card using restaurant object default data my solution for ex 109

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 import { Text } from "react-native";
+import { Card } from "react-native-paper";
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
     name = "Pinellas Ale House",
     icon,
     photos = [
-      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgraziadaily.co.uk%2Fcelebrity%2Fnews%2Fkate-middleton%2F&psig=AOvVaw1alxTsBlnKE_vx5CnOSDDi&ust=1642021288439000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDJ747MqvUCFQAAAAAdAAAAABAn",
+      "https://cdn.onebauer.media/one/lifestyle-images/celebrity/58aae0a65182f50c2a1ef301/kate-middleton-hero.jpg?format=jpg&quality=80&width=1400&ratio=16-9&resize=aspectfill",
     ],
     address = "100 some random st",
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
-  return <Text>{name}</Text>;
+  return (
+    <Card>
+      <Card.Cover source={{ url: `${photos[0]}` }} style={{ padding: 20 }} />
+      <Card.Title title={name} />
+    </Card>
+  );
 };
