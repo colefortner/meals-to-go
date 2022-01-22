1a5ccc431021fa90de01bda1ee032884c0d36e4d
Cole Fortner <colefortner@gmail.com>
2022-01-22 11:06:20 -0500

    Add loading animation by implementing ActivityIndicator from react-native-paper.  My solution for video 157

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 import { FlatList } from "react-native";
 import styled from "styled-components/native";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
@@ -25,6 +26,14 @@ export const RestaurantsScreen = () => {
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
+      {isLoading && (
+        <ActivityIndicator
+          animating={true}
+          color={Colors.blue800}
+          size={50}
+          style={{ marginTop: 200 }}
+        />
+      )}
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
