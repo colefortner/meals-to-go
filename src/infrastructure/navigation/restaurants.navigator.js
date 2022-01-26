6a4337213c7df134f0e816b9f31a44d78a79ee41
Cole Fortner <colefortner@gmail.com>
2022-01-26 16:02:51 -0500

    Add logic to render corresponding RestaurantInfoCard data in a detail screen when user presses a restaurant card on landing screeen

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -7,6 +7,8 @@ import {
 import { Text } from "react-native";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurant.screen";
+import { Spacer } from "../../components/spacer/spacer.component";
+import { RestaurantInfoCard } from "../../features/restaurants/components/restaurant-info-card.component";
 
 const RestaurantStack = createStackNavigator();
 
@@ -22,7 +24,15 @@ export const RestaurantsNavigator = () => {
       />
       <RestaurantStack.Screen
         name="RestaurantDetail"
-        component={() => <Text>Restaurant Detail</Text>}
+        component={({ route, navigation }) => {
+          const { restaurant } = route.params;
+
+          return (
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard restaurant={restaurant} />
+            </Spacer>
+          );
+        }}
       />
     </RestaurantStack.Navigator>
   );
