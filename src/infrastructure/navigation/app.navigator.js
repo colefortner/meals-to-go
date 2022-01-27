856eea636ccab3f60f4e32691c26132ee8856f47
Cole Fortner <colefortner@gmail.com>
2022-01-26 23:55:31 -0500

    Install react-native-maps and create an initial map view that the user navigates to when they click the map navigation tab

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -6,6 +6,7 @@ import { Text } from "react-native";
 
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { RestaurantsNavigator } from "./restaurants.navigator";
+import { MapScreen } from "../../features/map/screens/map.screens";
 
 const Tab = createBottomTabNavigator();
 
@@ -50,7 +51,7 @@ export const AppNavigator = () => {
         }}
       >
         <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-        <Tab.Screen name="Map" component={Maps} />
+        <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
