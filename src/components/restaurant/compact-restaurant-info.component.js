75e7a07de243d1284daac97269a1c8720dadf526
Cole Fortner <colefortner@gmail.com>
2022-01-28 23:44:08 -0500

    Implement WebView to handle the android bug of no image showing in our compact restaurant cards

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,9 +1,16 @@
 import React from "react";
 import styled from "styled-components/native";
+import WebView from "react-native-webview";
 
 import { Text } from "../typography/text.component";
 
 const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebview = styled(WebView)`
   border-radius: 10px;
   width: 120px;
   height: 100px;
@@ -15,10 +22,13 @@ const Item = styled.View`
   align-items: center;
 `;
 
+const isAndroid = Platform.OS === "android";
+
 export const CompactRestaurantInfo = ({ restaurant }) => {
+  const Image = isAndroid ? CompactWebview : CompactImage;
   return (
     <Item>
-      <CompactImage source={{ uri: restaurant.photos[0] }} />
+      <Image source={{ uri: restaurant.photos[0] }} />
       <Text center variant="caption" numberOfLines={3}>
         {restaurant.name}
       </Text>
